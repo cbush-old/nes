0c7b1e602a1578ea311d25afd8315e43601ffffa
crust <chrbsh@gmail.com>
2013-07-09 23:02:18 -0400

    woo

--- ppu.cpp
+++ ppu.cpp
@@ -10,6 +10,52 @@ using std::ifstream;
 using std::exception;
 using std::runtime_error;
 
+static uint32_t bisqwit_ntsc_pixel(unsigned pixel, int offset)
+{
+  // The input value is a NES color index (with de-emphasis bits).
+  // We need RGB values. To produce a RGB value, we emulate the NTSC circuitry.
+  // For most part, this process is described at:
+  //    http://wiki.nesdev.com/w/index.php/NTSC_video
+  // Incidentally, this code is shorter than a table of 64*8 RGB values.
+  static uint32_t palette[3][64][512] = {}, prev=~0u;
+  // Caching the generated colors
+  if(prev == ~0u)
+      for(int o=0; o<3; ++o)
+      for(int u=0; u<3; ++u)
+      for(int p0=0; p0<512; ++p0)
+      for(int p1=0; p1<64; ++p1)
+      {
+          // Calculate the luma and chroma by emulating the relevant circuits:
+          auto s = "\372\273\32\305\35\311I\330D\357}\13D!}N";
+          int y=0, i=0, q=0;
+          for(int p=0; p<12; ++p) // 12 samples of NTSC signal constitute a color.
+          {
+              // Sample either the previous or the current pixel.
+              int r = (p+o*4)%12, pixel = r < 8-u*2 ? p0 : p1; // Use pixel=p0 to disable artifacts.
+              // Decode the color index.
+              int c = pixel%16, l = c<0xE ? pixel/4 & 12 : 4, e=p0/64;
+              // NES NTSC modulator (square wave between up to four voltage levels):
+              int b = 40 + s[(c > 12*((c+8+p)%12 < 6)) + 2*!(0451326 >> p/2*3 & e) + l];
+              // Ideal TV NTSC demodulator:
+              y += b;
+              i += b * int(std::cos(M_PI * p / 6) * 5909);
+              q += b * int(std::sin(M_PI * p / 6) * 5909);
+          }
+          // Convert the YIQ color into RGB
+          auto gammafix = [=](float f) { return f <= 0.f ? 0.f : std::pow(f, 2.2f / 1.8f); };
+          auto clamp    = [](int v) { return v>255 ? 255 : v; };
+          // Store color at subpixel precision
+          if(u==2) palette[o][p1][p0] += 0x10000*clamp(255 * gammafix(y/1980.f + i* 0.947f/9e6f + q* 0.624f/9e6f));
+          if(u==1) palette[o][p1][p0] += 0x00100*clamp(255 * gammafix(y/1980.f + i*-0.275f/9e6f + q*-0.636f/9e6f));
+          if(u==0) palette[o][p1][p0] += 0x00001*clamp(255 * gammafix(y/1980.f + i*-1.109f/9e6f + q* 1.709f/9e6f));
+      }
+  
+  prev = pixel;
+  // Return RGB color
+  return (palette[offset][prev%64][pixel] << 8) | 0xff;
+  
+}
+
 template<int X, char X_MOD_8, bool TDM, bool X_ODD_64_TO_256, bool X_LT_256>
 static const void render2(PPU& ppu){
   #define loopy_w ppu.loopy_w
@@ -308,19 +354,21 @@ void PPU::render_pixel(){
   
   // this is so bisqwit!
   
-  bool edge = uint8_t(cycle + 8) < 16;
+  bool edge = uint8_t(cycle + 8) < 16; // 0..7, 248..255
   bool 
     showbg = ((!edge) || reg.show_bg8) && reg.show_bg,
     showsp = ((!edge) || reg.show_sp8) && reg.show_sp;
 
-  unsigned fx = scroll.xfine,
+
+  unsigned 
+    fx = scroll.xfine,
     xpos = 15 - (( (cycle&7) + fx + 8 * bool(cycle&7) ) & 15);
     
   unsigned pixel { 0 }, attr { 0 };
   
   if(showbg){
     pixel = (bg_shift_pat >> (xpos * 2)) & 3;
-    attr = (bg_shift_attr >> (xpos * 2)) & (!!pixel * 3);
+    attr = (bg_shift_attr >> (xpos * 2)) & (pixel ? 3 : 0);
   } else if((vram.raw & 0x3f00) == 0x3f00 && !reg.rendering_enabled){
     pixel = vram.raw;
   }
@@ -358,9 +406,14 @@ void PPU::render_pixel(){
 
   pixel = palette[(attr * 4 + pixel) & 0x1f] & (0x30 + (!reg.grayscale) * 0x0f);
 
-//  bisqwit_putpixel(cycle, scanline, pixel | (reg.intensify_rgb << 6), 0);
   
+  
+  #ifndef USE_BISQWIT_NTSC
+  framebuffer[scanline * 256 + cycle] = bisqwit_ntsc_pixel(pixel | ((reg.intensify_rgb)<<6), cycle%3);
+  #else
   framebuffer[scanline * 256 + cycle] = RGB[pixel&0x3f];
+  #endif
+  
   
 }
 
