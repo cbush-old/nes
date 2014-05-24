b02c1cdaec6e84dbc5cb74ab2e2b0a1bf720bd0a
cbush <cbush@macadamian.com>
2014-05-24 18:58:08 -0400

    RNG input...

---
+++ include/input_rng.h
@@ -0,0 +1,22 @@
+#ifndef INPUT_RNG_H
+#define INPUT_RNG_H
+
+#include "bus.h"
+
+#include <iostream>
+#include <random>
+
+class RNGInputDevice : public IInputDevice {
+  public:
+    RNGInputDevice(IController& controller);
+    ~RNGInputDevice(){}
+
+  public:
+    void tick();
+
+  private:
+    IController& port;
+    std::mt19937 gen;
+};
+
+#endif
---
+++ src/input_rng.cpp
@@ -0,0 +1,16 @@
+#include "input_rng.h"
+
+#include <ctime>
+#include <iostream>
+
+RNGInputDevice::RNGInputDevice(IController& controller)
+  : port(controller)
+  {
+    gen.seed(time(0));
+  }
+
+void RNGInputDevice::tick() {
+  IController::Button n = (IController::Button)(gen() % IController::N_BUTTONS);
+  int on = gen() & 2;
+  port.set_button_state(n, on);
+}
