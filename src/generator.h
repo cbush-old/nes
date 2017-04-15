9398ca15f6c3f5c2125a9aaf0efea775ee4a1317
cbush <git@gash.ca>
2017-04-15 00:03:06 -0400

    bit class is too slow for APU

--- src/generator.h
+++ src/generator.h
@@ -22,57 +22,68 @@ public:
 protected:
     ~Generator();
 
+    // reg0 // bit_index, bit_count
+    union
+    {
+        uint8_t _volume; // 0, 4
+        uint8_t _divider_period;
+        uint8_t _frequency_index;
+    };
+    bool _constant_volume; // 4, 1
+    union
+    {
+        bool _length_counter_halt; // 5, 1
+        bool _envelope_loop;
+    };
+    bool _loop_sample; // 6, 1
+    uint8_t _duty; // 6, 2
+    uint8_t _linear_counter_reload_value; // 0, 7
+    union
+    {
+        bool _triangle_length_counter_halt; // 7, 1
+        bool _triangle_control;
+        bool _enable_IRQ;
+    };
+    
+    // reg2
+    union
+    {
+        uint8_t _timer_low; // 16, 8
+        uint8_t _sample_address;
+    };
+    uint16_t _timer; // 16, 11
+    uint8_t _noise_period; // 16, 4
+    bool _noise_mode; // 23, 1
+
     union
     {
         // Common
-        bit<0, 8> reg0;
         bit<8, 8> reg1;
-        bit<16, 8> reg2;
         bit<24, 8> reg3;
 
-        // Pulse and noise
-        bit<0, 4> volume;
-        bit<0, 4> divider_period;
-        bit<4, 1> constant_volume;
-        bit<5, 1> length_counter_halt;
-        bit<5, 1> envelope_loop;
-
         // Pulse only
-        bit<6, 2> duty;
         bit<8, 3> sweep_shift;
         bit<11, 1> sweep_negative;
         bit<12, 3> sweep_period;
         bit<15, 1> sweep_enabled;
 
         // Pulse and triangle
-        bit<16, 11> timer;
-        bit<16, 8> timer_low;
         bit<24, 3> timer_high;
 
         // Pulse, triangle and noise
         bit<27, 5> length_counter_load;
 
-        // Triangle only
-        bit<0, 7> linear_counter_reload_value;
-        bit<7, 1> triangle_length_counter_halt;
-        bit<7, 1> triangle_control;
-
-        // Noise only
-        bit<16, 4> noise_period;
-        bit<23, 1> noise_mode;
-
         // DMC only
-        bit<0, 4> frequency_index;
-        bit<6, 1> loop_sample;
-        bit<7, 1> IRQ_enable;
         bit<8, 7> output_level;
-        bit<16, 8> sample_address;
         bit<24, 8> sample_length;
     };
 
     bool _enabled{ true };
     uint8_t _sample{0};
-    
+
+protected:
+    void set_timer(uint16_t value);
+
 private:
     virtual void reg1_write(uint8_t value);
 
